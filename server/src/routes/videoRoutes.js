5b19e870138a1b212c744fc2e520bd369a600b7d
Kike-Elvira <joseelvirarivas01@gmail.com>
2023-11-01 11:17:20 -0600

    v(0.5)

--- server/src/routes/videoRoutes.js
+++ server/src/routes/videoRoutes.js
@@ -16,10 +16,58 @@ router.get("/select_videos", (req, res) => {
   });
 });
 
-router.get('/reproducir-video/:nombreVideo', (req, res) => {
-    const videoName = req.params.nombreVideo; // Parámetro que especifica el nombre del video
-    const videoUrl = `/root/${videoName}`; // Construye la URL del video
-    res.json({ videoUrl });
+router.get("/reproducir-video/:nombreVideo", (req, res) => {
+  const videoName = req.params.nombreVideo; // Parámetro que especifica el nombre del video
+  const videoUrl = `/root/${videoName}`; // Construye la URL del video
+  res.json({ videoUrl });
+});
+
+router.get("/catalogo", (req, res) => {
+  const query = "SELECT * FROM Catalogo";
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Error en la base de datos" });
+    }
+
+    return res.status(200).json(results); // Devuelve los videos como respuesta en formato JSON
+  });
+});
+
+router.get("/catalogo_Super", (req, res) => {
+    const query = "SELECT * FROM Contenido WHERE Genero LIKE '%Super Heroes%'";
+    db.query(query, (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Error en la base de datos" });
+      }
+  
+      return res.status(200).json(results); // Devuelve los videos como respuesta en formato JSON
+    });
+  });
+
+  router.get("/catalogo_anime", (req, res) => {
+    const query = "SELECT * FROM Contenido WHERE Genero LIKE '%Anime%'";
+    db.query(query, (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Error en la base de datos" });
+      }
+  
+      return res.status(200).json(results); // Devuelve los videos como respuesta en formato JSON
+    });
+  });
+
+  router.get("/catalogo_terror", (req, res) => {
+    const query = "SELECT * FROM Contenido WHERE Genero LIKE '%Terror%'";
+    db.query(query, (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Error en la base de datos" });
+      }
+  
+      return res.status(200).json(results); // Devuelve los videos como respuesta en formato JSON
+    });
   });
 
 module.exports = router;
